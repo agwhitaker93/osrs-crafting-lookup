21c715d2e939d93a3f525bd3e69c0062259dc6fc
Andrew Whitaker <agwhitaker93@gmail.com>
2020-05-09 07:25:58 +0100

    small changes

--- src/clj/osrs_crafting_lookup/styles.clj
+++ src/clj/osrs_crafting_lookup/styles.clj
@@ -7,23 +7,23 @@
 (def nav-button-width "4rem")
 
 (defstyles style
-  [:.nav-bar {:position "absolute"
-              :top 0
-              :left 0
-              :height nav-height
-              :width "100%"
-              :background-color "#85506e"}]
-  [:.nav-inner {:position "absolute"
-                :top "50%"
-                :transform "translateY(-50%)"}]
-  [:.nav-home {:left side-dist
-               :height el-height
-               :line-height el-height
-               :width "4rem"
-               :text-align "center"}]
-  [:.nav-search {:right (str "calc(" side-dist " + " nav-button-width ")")
-                 :height el-height}]
-  [:.nav-search-button {:right side-dist
-                        :height el-height
-                        :width nav-button-width}]
-  [:.results {:padding-top nav-height}])
+           [:body {:padding-top nav-height}]
+           [:.nav-bar {:position         "absolute"
+                       :top              0
+                       :left             0
+                       :height           nav-height
+                       :width            "100%"
+                       :background-color "#85506e"}]
+           [:.nav-inner {:position  "absolute"
+                         :top       "50%"
+                         :transform "translateY(-50%)"}]
+           [:.nav-home {:left        side-dist
+                        :height      el-height
+                        :line-height el-height
+                        :width       "4rem"
+                        :text-align  "center"}]
+           [:.nav-search {:right  (str "calc(" side-dist " + " nav-button-width ")")
+                          :height el-height}]
+           [:.nav-search-button {:right  side-dist
+                                 :height el-height
+                                 :width  nav-button-width}])
