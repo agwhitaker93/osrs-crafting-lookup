af588ded3daf8ee9ec8e7b2bef486b9b58939fee
Andrew Whitaker <agwhitaker93@gmail.com>
2020-05-30 18:46:29 +0100

    begin implementing chainable material and product lookups, broke how the target materials stuff works though

--- src/clj/osrs_crafting_lookup/components/recipes.clj
+++ src/clj/osrs_crafting_lookup/components/recipes.clj
@@ -5,11 +5,15 @@
 (defn get-skills-for-recipe [{id :id recipe-id :recipe_id}]
   (db/get-recipe-skills id recipe-id))
 
-(defn get-materials-for-recipe [{id :id recipe-id :recipe_id} material-depth]
+(defn get-materials-for-recipe [{id :id recipe-id :recipe_id}]
   (db/get-recipe-materials id recipe-id))
 
-(defn get-products-for-recipe [recipe-name product-depth]
+(defn get-products-for-recipe [recipe-name]
   (db/get-recipe-products recipe-name))
+
+(defn get-product-details [product depth])
+
+(defn get-material-details [material depth])
 
 (defn get-recipe [{id :id
                    name :name
@@ -17,16 +21,26 @@
                    product-depth :product-depth
                    :or {material-depth 0
                         product-depth 0}}]
-  (let [get-item-fn (if (not (nil? id))
+  (let [material-depth (parse-int material-depth)
+        product-depth (parse-int product-depth)
+        get-item-fn (if (not (nil? id))
                       #(db/get-item-details id)
                       #(db/get-item-details-by-name name))
         item-details (first (get-item-fn))
-        recipe-details (map #(assoc %1
-                                    :skills (get-skills-for-recipe %1)
-                                    :materials (get-materials-for-recipe %1 material-depth))
-                            (db/get-recipe-details (:id item-details)))]
-    {:body {:target   (assoc item-details :recipes recipe-details)
-            :products (get-products-for-recipe (:name item-details) product-depth)}}))
+        recipes (db/get-recipe-details (:id item-details))
+        skills (map #(get-skills-for-recipe %1) recipes)
+        materials (map #(get-materials-for-recipe %1) recipes)
+        recipes (map #(assoc %1
+                             :skills skills
+                             :materials materials) recipes)
+        products (get-products-for-recipe (:name item-details))]
+    (as-> {:body {:target   (assoc item-details :recipes recipes :products products)}} $
+      (if (> product-depth 0)
+        (assoc $ :products (map #(get-product-details %1 (dec product-depth)) products))
+        $)
+      (if (> material-depth 0)
+        (assoc $ :materials (map #(get-material-details %1 (dec material-depth)) materials))
+        $))))
 
 (defn get-recipes [{name :name limit :limit page :page :or {limit 15 page 1} :as input}]
   (let [limit (parse-int limit)
